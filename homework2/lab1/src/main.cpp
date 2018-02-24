22c853c1d5f33ab70aa9cbbe60e9202bdcc5cde8
Alberto Cenzato <alberto.cenzato@outlook.it>
2018-02-24 11:54:15 +0100

    Refactoring and preparing for homework 2

---
+++ homework2/lab1/src/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by alberto on 24/02/18.
+//
+
+#include <iostream>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+int main(int argc, char** argv) {
+
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+
+    std::cout << "Hello cloud World!" << std::endl;
+
+    return 0;
+}

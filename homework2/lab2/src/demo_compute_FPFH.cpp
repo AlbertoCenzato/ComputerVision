530877241ded355485deed015161d019b3ec56ea
Alberto Cenzato <alberto.cenzato@outlook.it>
2018-03-01 11:24:11 +0100

    Lab2 ready

--- homework2/lab2/src/demo_compute_FPFH.cpp
+++ homework2/lab2/src/demo_compute_FPFH.cpp
@@ -14,6 +14,7 @@
 #include <pcl/features/normal_3d_omp.h>	// for computing normals with multi-core implementation
 #include <pcl/features/fpfh_omp.h>		// for computing FPFH with multi-core implementation
 #include <pcl/filters/filter.h>		// for removing NaN from the point cloud
+#include <pcl/keypoints/sift_keypoint.h>
 
 
 struct callbackArgs{
@@ -24,12 +25,83 @@ struct callbackArgs{
 
 void pp_callback (const pcl::visualization::PointPickingEvent& event, void* args) {
 	// callback function used to visualize feature histograms
-	auto data = std::static_cast<struct callbackArgs *>(args);
+	auto data = static_cast<struct callbackArgs *>(args);
     if (event.getPointIndex() == -1)
       return;
     std::stringstream windowName;
     windowName << "FPFH for point " << event.getPointIndex();
     data->histViewer->addFeatureHistogram (*(data->fpfhs), "fpfh", event.getPointIndex(), windowName.str(), 640, 200);
+}
+
+
+pcl::PointCloud<pcl::PointNormal>::Ptr computeNormals(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud) {
+    // Create the normal estimation class, and pass the input dataset to it
+    pcl::NormalEstimationOMP<pcl::PointXYZRGB, pcl::PointNormal> ne;
+    ne.setInputCloud(cloud);
+
+    // Create an empty kdtree representation, and pass it to the normal estimation object.
+    // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>());
+    ne.setSearchMethod(tree);
+
+    // Output datasets
+    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_normals(new pcl::PointCloud<pcl::PointNormal>);
+
+    // Use all neighbors in a sphere of radius 3cm
+    ne.setRadiusSearch(0.03);
+
+    // Compute the features
+    std::cout << "Computing normals...please wait..." << std::flush;
+    auto numOfThreads = std::thread::hardware_concurrency();
+    ne.setNumberOfThreads(numOfThreads); 	// set number of threads when using OpenMP
+    ne.compute (*cloud_normals);
+    std::cout << "done." << std::endl;
+
+    return cloud_normals;
+}
+
+
+pcl::PointCloud<pcl::PointWithScale>::Ptr computeSIFT(pcl::PointCloud<pcl::PointNormal>::Ptr &cloud_normals) {
+    const float min_scale = 0.01f;
+    const int n_octaves = 3;
+    const int n_scales_per_octave = 4;
+    const float min_contrast = 0.001f;
+
+    pcl::SIFTKeypoint<pcl::PointNormal, pcl::PointWithScale> sift;
+    pcl::PointCloud<pcl::PointWithScale>::Ptr result(new pcl::PointCloud<pcl::PointWithScale>);
+    pcl::search::KdTree<pcl::PointNormal>::Ptr tree(new pcl::search::KdTree<pcl::PointNormal>());
+    sift.setSearchMethod(tree);
+    sift.setScales(min_scale, n_octaves, n_scales_per_octave);
+    sift.setMinimumContrast(min_contrast);
+    sift.setInputCloud(cloud_normals);
+    sift.compute(*result);
+
+    return result;
+}
+
+
+pcl::PointCloud<pcl::FPFHSignature33>::Ptr computeFPFH(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud, pcl::PointCloud<pcl::PointNormal>::Ptr &cloud_normals) {
+    // Create the FPFH estimation class, and pass the input dataset+normals to it
+    pcl::FPFHEstimationOMP<pcl::PointXYZRGB, pcl::PointNormal, pcl::FPFHSignature33> fpfh;
+    fpfh.setInputCloud(cloud);
+    fpfh.setInputNormals(cloud_normals);
+    // alternatively, if cloud is of tpe PointNormal, do fpfh.setInputNormals (cloud);
+
+    // Output datasets
+    pcl::PointCloud<pcl::FPFHSignature33>::Ptr fpfhs(new pcl::PointCloud<pcl::FPFHSignature33>());
+
+    // Use all neighbors in a sphere of radius 5cm
+    // IMPORTANT: the radius used here has to be larger than the radius used to estimate the surface normals!!!
+    fpfh.setRadiusSearch(0.05);
+
+    // Compute the features
+    std::cout << "Computing FPFH features...please wait..." << std::flush;
+    auto numOfThreads = std::thread::hardware_concurrency();
+    fpfh.setNumberOfThreads(numOfThreads); 	// set number of threads when using OpenMP
+    fpfh.compute (*fpfhs);
+    std::cout << "done." << std::endl;
+
+    return fpfhs;
 }
 
 int main (int argc, char** argv) {
@@ -38,7 +110,7 @@ int main (int argc, char** argv) {
 	pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal> ());
 
 	// Load point cloud from .pcd file:
-	if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("../dataset/minimouse1_segmented.pcd", *cloud) == -1) {
+	if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("../dataset_lab2/minimouse1_segmented.pcd", *cloud) == -1) {
 		PCL_ERROR ("Couldn't read the pcd file \n");
 		return (-1);
 	}
@@ -50,49 +122,13 @@ int main (int argc, char** argv) {
 	std::vector<int> indices;
 	pcl::removeNaNFromPointCloud(*cloud, *cloud, indices);
 
-	// Create the normal estimation class, and pass the input dataset to it
-	//  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-	pcl::NormalEstimationOMP<pcl::PointXYZRGB, pcl::Normal> ne;
-	ne.setInputCloud(cloud);
+    auto cloud_normals = computeNormals(cloud);
 
-	// Create an empty kdtree representation, and pass it to the normal estimation object.
-	// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
-	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB> ());
-	ne.setSearchMethod(tree);
+    auto keyPoints = computeSIFT(cloud_normals);
 
-	// Output datasets
-	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
-
-	// Use all neighbors in a sphere of radius 3cm
-	ne.setRadiusSearch(0.03);
-
-	// Compute the features
-  	std::cout << "Computing normals...please wait..." << std::flush;
-	auto numOfThreads = std::thread::hardware_concurrency();
-	ne.setNumberOfThreads(numOfThreads); 	// set number of threads when using OpenMP
-	ne.compute (*cloud_normals);
-	std::cout << "done." << std::endl;
+    auto fpfhs = computeFPFH(cloud, cloud_normals);
 
 	// cloud_normals->points.size () should have the same size as the input cloud->points.size ()*
-
-	// Create the FPFH estimation class, and pass the input dataset+normals to it
-	pcl::FPFHEstimationOMP<pcl::PointXYZRGB, pcl::Normal, pcl::FPFHSignature33> fpfh;
-	fpfh.setInputCloud(cloud);
-	fpfh.setInputNormals(cloud_normals);
-	// alternatively, if cloud is of tpe PointNormal, do fpfh.setInputNormals (cloud);
-
-	// Output datasets
-	pcl::PointCloud<pcl::FPFHSignature33>::Ptr fpfhs(new pcl::PointCloud<pcl::FPFHSignature33> ());
-
-	// Use all neighbors in a sphere of radius 5cm
-	// IMPORTANT: the radius used here has to be larger than the radius used to estimate the surface normals!!!
-	fpfh.setRadiusSearch (0.05);
-
-	// Compute the features
-  	std::cout << "Computing FPFH features...please wait..." << std::flush;
-	fpfh.setNumberOfThreads(numOfThreads); 	// set number of threads when using OpenMP
-	fpfh.compute (*fpfhs);
-	std::cout << "done." << std::endl;
 
 	// Visualize FPFH:
 	int normalsVisualizationStep = 100; // to visualize a normal every normalsVisualizationStep
@@ -105,7 +141,7 @@ int main (int argc, char** argv) {
 	viewer.initCameraParameters ();
 	pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
 	viewer.addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "input_cloud");
-	viewer.addPointCloudNormals<pcl::PointXYZRGB, pcl::Normal>(cloud, cloud_normals, normalsVisualizationStep, normalsScale, "normals");
+	viewer.addPointCloudNormals<pcl::PointXYZRGB, pcl::PointNormal>(cloud, cloud_normals, normalsVisualizationStep, normalsScale, "normals");
 
 	// Create structure containing the parameters for the callback function
 	struct callbackArgs histCallbackArgs;
@@ -113,7 +149,7 @@ int main (int argc, char** argv) {
 	histCallbackArgs.fpfhs = fpfhs;
 
 	// Add point picking callback to viewer (for visualizing feature histograms):
-	viewer.registerPointPickingCallback(pp_callback, std::static_cast<void*>(&histCallbackArgs));
+	viewer.registerPointPickingCallback(pp_callback, static_cast<void*>(&histCallbackArgs));
 
 	// Loop for visualization (so that the visualizers are continuously updated):
 	std::cout << "Visualization... "<< std::endl;
